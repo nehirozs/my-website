c212b428d084d372f3a1da900732d2d5b3ef400a
Nehir Özsunar <nehirozsunar@nehirs-mbp-001.station>
2025-12-21 05:18:54 -0500

    logo c2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ export default function Home() {
           {[
             { name: 'Python', logo: pythonLogo },
             { name: 'Java', logo: javaLogo },
-            { name: 'C#', logo: cLogo },
+            { name: 'C', logo: cLogo },
             { name: 'OCaml', logo: ocamlLogo },
             { name: 'Bash', logo: bashLogo },
             { name: 'MIPS', logo: mipsLogo },
